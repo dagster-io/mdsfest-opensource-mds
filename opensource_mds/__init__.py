ca5fc6ed7d099b720c81c4f2e9b0ad822a99eaa6
erikjamesmason <ejmdpg@gmail.com>
2023-11-28 19:30:51 -0800

    minor change for uniformity with resources

--- opensource_mds/__init__.py
+++ opensource_mds/__init__.py
@@ -4,12 +4,14 @@ from . import assets, resources
 
 all_assets = load_assets_from_modules([assets])
 duckdb_resource = resources.duckdb_resource
+sling_resource = resources.sling_resource
+dbt_resource = resources.dbt_resource
 
 defs = Definitions(
     assets=all_assets,
     resources={
         "duckdb": duckdb_resource,
-        "sling": resources.sling_resource,
-        "dbt": resources.dbt_resource,
+        "sling": sling_resource,
+        "dbt": dbt_resource,
     },
 )
