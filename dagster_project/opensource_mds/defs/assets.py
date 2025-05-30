072ab462612f8321e9a3e0e325d4692b0911b3e4
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2025-05-30 09:14:54 -0700

    everything is component

--- dagster_project/opensource_mds/defs/assets.py
+++ dagster_project/opensource_mds/defs/assets.py
@@ -1,4 +1,3 @@
-
 import dagster as dg
 from dagster_dbt import DbtCliResource, dbt_assets
 from dagster_duckdb import DuckDBResource
@@ -9,130 +8,11 @@ from dagster_sling import (
 
 from opensource_mds.defs.resources import CustomDagsterDbtTranslator, dbt_manifest_path
 
-from . import constants
-
 retry_policy = dg.RetryPolicy(
     max_retries=3,
     delay=0.2,  # 200ms
     backoff=dg.Backoff.EXPONENTIAL,
 )
-
-
-@dg.asset(
-    deps=[dg.AssetKey("checklist_2020"), dg.AssetKey("checklist_2023")],
-    compute_kind="duckdb",
-    group_name="prepared",
-    retry_policy=retry_policy,
-)
-def birds(context: dg.AssetExecutionContext, duckdb: DuckDBResource):
-    cl2020 = dg.file_relative_path(__file__, constants.CL_2020_FPATH)
-    cl2023 = dg.file_relative_path(__file__, constants.CL_2023_FPATH)
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            f"""CREATE OR REPLACE TABLE birds_2020_tmp AS (
-                    SELECT * FROM read_csv_auto('{cl2020}', 
-                        sample_size=-1))
-            """
-        )
-        conn.execute(
-            f"""CREATE OR REPLACE TABLE birds_2023_tmp AS (
-                    SELECT * FROM read_csv_auto('{cl2023}',
-                        sample_size=-1))
-            """
-        )
-        conn.execute(
-            """ CREATE OR REPLACE TABLE birds as (
-                 SELECT * from birds_2020_tmp
-                 UNION ALL
-                 SELECT * from birds_2023_tmp
-                )
-            """
-        )
-        nrows = conn.execute("SELECT COUNT(*) FROM birds").fetchone()[0]  # type: ignore
-
-        metadata = conn.execute(
-            "select * from duckdb_tables() where table_name = 'birds'"
-        ).pl()
-
-    context.log.info("Created birds table")
-    context.add_output_metadata(
-        metadata={
-            "num_rows": nrows,
-            "table_name": metadata["table_name"][0],
-            "datbase_name": metadata["database_name"][0],
-            "schema_name": metadata["schema_name"][0],
-            "column_count": metadata["column_count"][0],
-            "estimated_size": metadata["estimated_size"][0],
-        }
-    )
-
-
-@dg.asset(
-    deps=["species_translation_data"],
-    compute_kind="duckdb",
-    group_name="prepared",
-    retry_policy=retry_policy,
-)
-def species(context: dg.AssetExecutionContext, duckdb: DuckDBResource):
-    species = dg.file_relative_path(__file__, constants.SPECIES_TRANSLATION_FPATH)
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            f"""CREATE OR REPLACE TABLE species AS (
-                    SELECT * FROM read_csv_auto('{species}'))
-            """
-        )
-        nrows = conn.execute("SELECT COUNT(*) FROM species").fetchone()[0]  # type: ignore
-
-        metadata = conn.execute(
-            "select * from duckdb_tables() where table_name = 'species'"
-        ).pl()
-
-    context.add_output_metadata(
-        metadata={
-            "num_rows": nrows,
-            "table_name": metadata["table_name"][0],
-            "datbase_name": metadata["database_name"][0],
-            "schema_name": metadata["schema_name"][0],
-            "column_count": metadata["column_count"][0],
-            "estimated_size": metadata["estimated_size"][0],
-        }
-    )
-
-    context.log.info("Created species table")
-
-
-@dg.asset(
-    deps=["site_description_data"],
-    compute_kind="duckdb",
-    group_name="prepared",
-    retry_policy=retry_policy,
-)
-def sites(context: dg.AssetExecutionContext, duckdb: DuckDBResource):
-    sites = dg.file_relative_path(__file__, constants.SITE_DATA_FPATH)
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            f"""CREATE OR REPLACE TABLE sites AS (
-                    SELECT * FROM read_csv_auto('{sites}'))
-            """
-        )
-        nrows = conn.execute("SELECT COUNT(*) FROM sites").fetchone()[0]  # type: ignore
-
-        metadata = conn.execute(
-            "select * from duckdb_tables() where table_name = 'sites'"
-        ).pl()
-
-    context.add_output_metadata(
-        metadata={
-            "num_rows": nrows,
-            "table_name": metadata["table_name"][0],
-            "datbase_name": metadata["database_name"][0],
-            "schema_name": metadata["schema_name"][0],
-            "column_count": metadata["column_count"][0],
-            "estimated_size": metadata["estimated_size"][0],
-        }
-    )
-
-    context.log.info("Created sites table")
 
 
 replication_config = {
@@ -148,68 +28,6 @@ def sling_sync_assets(context, sling: SlingResource):
     yield from sling.replicate(context=context)
     for row in sling.stream_raw_logs():
         context.log.info(row)
-
-
-@dg.asset(
-    deps=[sling_sync_assets],
-    compute_kind="duckdb",
-    group_name="prepared",
-    retry_policy=retry_policy,
-)
-def tickets(context: dg.AssetExecutionContext, duckdb: DuckDBResource):
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            "CREATE OR REPLACE TABLE tickets AS (SELECT * FROM main.public_tickets)"
-        )
-        nrows = conn.execute("SELECT COUNT(*) FROM tickets").fetchone()[0]  # type: ignore
-
-        metadata = conn.execute(
-            "select * from duckdb_tables() where table_name = 'tickets'"
-        ).pl()
-
-    context.add_output_metadata(
-        metadata={
-            "num_rows": nrows,
-            "table_name": metadata["table_name"][0],
-            "datbase_name": metadata["database_name"][0],
-            "schema_name": metadata["schema_name"][0],
-            "column_count": metadata["column_count"][0],
-            "estimated_size": metadata["estimated_size"][0],
-        }
-    )
-
-    context.log.info("Created tickets table")
-
-
-@dg.asset(
-    deps=[sling_sync_assets],
-    compute_kind="duckdb",
-    group_name="prepared",
-    retry_policy=retry_policy,
-)
-def events(context: dg.AssetExecutionContext, duckdb: DuckDBResource):
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            "CREATE OR REPLACE TABLE events AS (SELECT * FROM main.public_events)"
-        )
-        nrows = conn.execute("SELECT COUNT(*) FROM events ").fetchone()[0]  # type: ignore
-
-        metadata = conn.execute(
-            "select * from duckdb_tables() where table_name = 'events'"
-        ).pl()
-
-    context.add_output_metadata(
-        metadata={
-            "num_rows": nrows,
-            "table_name": metadata["table_name"][0],
-            "datbase_name": metadata["database_name"][0],
-            "schema_name": metadata["schema_name"][0],
-            "column_count": metadata["column_count"][0],
-            "estimated_size": metadata["estimated_size"][0],
-        }
-    )
-
-    context.log.info("Created events table")
 
 
 @dbt_assets(
